44dcf2b3bcfd51b130a8a5146e2ec2411772e7bf
Ashmeik Ashmeik <ashmeik2maker@gmail.com>
2025-04-04 10:18:18 -0700

    Added helloWorld.c and generated assembly helloWorldFromC.s

---
+++ helloWorldc/helloWorld.c
@@ -0,0 +1,12 @@
+// File: helloWorld.c
+// Author: Ashmeik Ashmeik
+// Date: April 3, 2025
+// Purpose: Simple C program that prints "Hello, World!"
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
